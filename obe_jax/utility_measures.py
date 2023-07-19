5e2dcd69d6252a4f4a99319dd0a5cb5b229f3564
Paul Kairys <pkairys@anl.gov>
2023-07-19 11:09:06 -0400

    fixing utility functions

--- obe_jax/utility_measures.py
+++ obe_jax/utility_measures.py
@@ -5,8 +5,8 @@ from jax import jit
 def entropy_change(current_particles,current_weights,likelihoods):
     new_weights = current_weights*likelihoods
     new_weights = new_weights/jnp.sum(new_weights)
-    H_old = jnp.sum(current_weights*jnp.log2(current_weights))
-    H_new = jnp.sum(new_weights*jnp.log2(new_weights))
+    H_old = jnp.nansum(current_weights*jnp.log2(current_weights))
+    H_new = jnp.nansum(new_weights*jnp.log2(new_weights))
     return H_new-H_old
 
 @jit
@@ -14,7 +14,7 @@ def relative_entropy(current_particles,current_weights,likelihoods):
     new_weights = current_weights*likelihoods
     new_weights = new_weights/jnp.sum(new_weights)
     log_rel = jnp.log2(jnp.divide(current_weights,new_weights))
-    return -jnp.sum(new_weights*log_rel)
+    return -jnp.nansum(new_weights*log_rel)
 
 @jit
 def posterior_variance(current_particles,current_weights,likelihoods):
